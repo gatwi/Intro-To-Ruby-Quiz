09574417923c2249902d4b3eaa0f2a57a77d984d
gatwi <faith.murithi@student.moringaschool.com>
2022-08-26 11:03:15 +0300

    boolean data type

--- variables-and-data-types/app.rb
+++ variables-and-data-types/app.rb
@@ -3,7 +3,14 @@
 
 #a number to the value 25
 #a boolean to the value true 
+if true
+    puts "It is correct!"  
+else
+    puts "It is wrong!"
+end
 #a string to the value "Ruby"
+
+
 
 ##  Q2 Set values 100, 30 for the  variables cars and drivers respectively.
 #Output: There are 100 cars and 30 drivers.
