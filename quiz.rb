dd98d472c2eb165dc4a01c29d4ffba731e9f7bb5
katampoi <simonkatampoi@student.moringaschool.com>
2022-08-26 11:08:01 +0300

    reversed users input

--- quiz.rb
+++ quiz.rb
@@ -1,4 +1,15 @@
-puts "   /|"
-puts "  / |"
-puts " /  |"
-puts "/ _ |"
+# puts "   /|"
+# puts "  / |"
+# puts " /  |"
+# puts "/ _ |"
+
+
+##Q4 Write a Ruby program which accept the user's first and last name and print them in reverse order with a 
+#space between them.
+puts "Enter Your First Name: "
+firstName = gets.chomp()
+
+puts "Enter Your Last Name: "
+lastName = gets.chomp()
+
+puts ( firstName + lastName).reverse
