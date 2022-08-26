a48fc8a639db135eb308cf86abfdec65954dfbc6
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-08-26 12:05:02 +0300

    set variables

--- variables-and-data-types/app.rb
+++ variables-and-data-types/app.rb
@@ -2,10 +2,15 @@
 ## Q1 Set variables for the corresponding values:
 
 #a number to the value 25
-#a boolean to the value true 
+character_age = 25
+#a boolean to the value true
+isTall = true
 #a string to the value "Ruby"
+character_name = "Ruby"
 
 ##  Q2 Set values 100, 30 for the  variables cars and drivers respectively.
 #Output: There are 100 cars and 30 drivers.
+cars = 100
+drivers = 30
 
-
+puts "There are #{cars} cars and #{drivers} drivers."
